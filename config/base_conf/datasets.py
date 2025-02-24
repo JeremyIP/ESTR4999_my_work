cbb1d577aff349608928db4bbe6ef13a0c0c320b
Jeremy Ip <1155176223@link.cuhk.edu.hk>
2025-02-24 12:58:37 +0800

    Brief description of the changes made

--- config/base_conf/datasets.py
+++ config/base_conf/datasets.py
@@ -60,7 +60,8 @@ AAPL_conf = dict(
     freq=1440,                    # Daily frequency in minutes
     #data_split=[975, 31, 504],     # Example split: 755 days for training, 221 for testing
     # data_split=[754, 252, 504]
-    data_split=[754, 0, 100]
+    #data_split=[754, 0, 100]
+    data_split=[2000, 0, 1242]
 
 )
 
