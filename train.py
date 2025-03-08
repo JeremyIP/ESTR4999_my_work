9d8eeccf9ba4a26a51e0412872ce53f990889c9a
Jeremy Ip <1155176223@link.cuhk.edu.hk>
2025-03-08 15:22:24 +0800

    Brief description of the changes made

--- train.py
+++ train.py
@@ -193,7 +193,7 @@ def genetic_algorithm(training_conf, conf):
 
         table_each_generation = PrettyTable()
         table_each_generation.field_names = ["Chromosome ID", "Features", "Hyperparameters", "Fitness"]
-        table_each_generation.add_row([index+1, ''.join(str(bit) for bit in element.genes['features']), ''.join(str(bit) for bit in element.genes['hyperparameters']), element.fitness] for index, element in list(enumerate(list_ind)))
+        table_each_generation.add_rows([index+1, ''.join(str(bit) for bit in element.genes['features']), ''.join(str(bit) for bit in element.genes['hyperparameters']), element.fitness] for index, element in list(enumerate(list_ind)))
         table_each_generation.title = f"Generation {generation+1}"
         print(table_each_generation)
 
