04d81d8d1b5c78097f5df1e16d63582ab31fcca8
Jeremy Ip <1155176223@link.cuhk.edu.hk>
2025-03-08 16:03:34 +0800

    Brief description of the changes made

--- core/ltsf_runner.py
+++ core/ltsf_runner.py
@@ -118,9 +118,9 @@ class LTSFRunner(L.LightningModule):
 
     def training_step(self, batch, batch_idx):
         loss = self.loss_function(*self.forward(batch, batch_idx))
-        print(loss.shape)
         self.log('train/loss', loss, on_step=True, on_epoch=True, prog_bar=True, sync_dist=True)
         self.train_losses.append(loss.item())
+        print(loss)
         return loss
 
     def validation_step(self, batch, batch_idx):
